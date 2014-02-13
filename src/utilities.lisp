be929381607c7c80f3cfa765b35dc502d7bab5e4
Mark Fedurin <hitecnologys@gmail.com>
2014-02-13 08:14:06 +0700

    Changed DOARRAY to user iterate

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -1,10 +1,11 @@
 (in-package :jams)
 
 (defmacro doarray ((array counter element &optional size increment) &body body)
-  `(loop for ,counter upto ,(1- (if size (symbol-value size) (array-total-size array)))
-         by ,(if increment increment 1)
-         for ,element = (row-major-aref ,array ,counter)
-         do ,@body))
+  `(iter
+     (for ,counter to ,(1- (if size (symbol-value size) (array-total-size array)))
+          by ,(if increment increment 1))
+     (for ,element next (row-major-aref ,array ,counter))
+     (after-each ,@body)))
 
 (defun log-message (type message &rest arguments)
   (format t
