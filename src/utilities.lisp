695753b494247b896c38bb4f7e7820df3d6221b4
Mark Fedurin <hitecnologys@gmail.com>
2014-02-14 20:39:01 +0700

    Oops, fixed typo in DOARRAY

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -2,7 +2,7 @@
 
 (defmacro doarray ((array counter element &key size increment) &body body)
   `(iter
-     (for ,counter below (1- (or ,size `(array-total-size ,array)))
+     (for ,counter below (1- (or ,size (array-total-size ,array)))
           by (or ,increment 1))
      (for ,element next (row-major-aref ,array ,counter))
      (after-each ,@body)))
